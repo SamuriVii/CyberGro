e3212d6af5fff5d994e4df72d61e18d72d076b95
lightlead <d.koscielski@outlook.com>
2025-05-30 22:53:20 +0200

    Added SSL certificate for localhost

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import fs from 'fs';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  devServer: {
+    https: {
+      key: fs.readFileSync('./cert/localhost-key.pem').toString(),
+      cert: fs.readFileSync('./cert/localhost.pem').toString()
+    },
+    port: 3000,
+    host: 'localhost'
+  },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss']
 })
